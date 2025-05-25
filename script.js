9fe8ddee9370be26326560481336c653d351e8e3
eriksimarmata89 <42613244+eriksimarmata89@users.noreply.github.com>
2025-05-25 19:55:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ document.getElementById("form-pekerjaan").addEventListener("submit", function (e
     const formData = new FormData(form);
     const data = new URLSearchParams(formData);
 
-    fetch("https://script.google.com/macros/s/AKfycbywkqNEpDPrgDw5RdYhIivwjnEX7kjpKjWwfBuM20D-vrrbR7yQGL45qXQKrE2GSo3Khw/exec
-", { // Ganti dengan URL Web App kamu
+    fetch("https://script.google.com/macros/s/AKfycbywkqNEpDPrgDw5RdYhIivwjnEX7kjpKjWwfBuM20D-vrrbR7yQGL45qXQKrE2GSo3Khw/exec", { // Ganti dengan URL Web App kamu
       method: "POST",
       body: data,
     })
